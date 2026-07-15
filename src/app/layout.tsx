a9315d7d795b0d1d88460c73b9a00f031cd37e23
신정우 <inconcluso@dankook.ac.kr>
2026-07-15 09:23:46 +0900

    배경 이미지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,18 +33,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
-      <body style={{ background: '#F9F9FF', minHeight: '100vh', position: 'relative' }}>
-        <div style={{
-          position: 'fixed',
-          inset: 0,
-          backgroundImage: 'url(/back_image.png)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          opacity: 0.08,
-          zIndex: 0,
-          pointerEvents: 'none',
-        }} />
+      <body style={{ background: '#E8EBF5', minHeight: '100vh', position: 'relative' }}>
+  <div style={{
+    position: 'fixed',
+    inset: 0,
+    backgroundImage: 'url(/back_image.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    opacity: 0.3,
+    zIndex: 0,
+    pointerEvents: 'none',
+  }} />
         <div style={{ position: 'relative', zIndex: 1 }}>
           <ServiceWorkerRegister />
           {children}
